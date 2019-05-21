9c13258d371010a47d671f26e49630b716bfe05c
roxannethomas <roxthomas@auchan.fr>
2019-05-22 00:18:52 +0200

    API call implementation (beginning)

---
+++ app/src/main/java/com/projet/roxanne_thomas/betaserie/BetaseriesCall.java
@@ -0,0 +1,13 @@
+package com.projet.roxanne_thomas.betaserie;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Headers;
+
+public interface BetaseriesCall
+{
+    @Headers({
+            "X-BetaSeries-Version: 3.0",
+            "X-BetaSeries-Key: dae7ecdb03b9"
+    })
+}
--- app/src/main/java/com/projet/roxanne_thomas/betaserie/FicheSerieActivity.java
+++ app/src/main/java/com/projet/roxanne_thomas/betaserie/FicheSerieActivity.java
@@ -1,7 +1,14 @@
 package com.projet.roxanne_thomas.betaserie;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class FicheSerieActivity
+public class FicheSerieActivity  extends AppCompatActivity
 {
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fiche_serie_activity);
+    }
 }
