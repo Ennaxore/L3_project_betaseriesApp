902f69ed5e70e10df611f7729104a2bd51f69db2
roxannethomas <roxthomas@auchan.fr>
2019-05-23 09:46:26 +0200

    Top20Series creation

---
+++ app/src/main/java/com/projet/roxanne_thomas/betaserie/Top20Series.java
@@ -0,0 +1,37 @@
+package com.projet.roxanne_thomas.betaserie;
+
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class Top20Series
+{
+    @SerializedName("posters")
+    @Expose
+    private List<String> posters = null;
+    @SerializedName("errors")
+    @Expose
+    private List<Object> errors = null;
+
+    public List<String> getPosters()
+    {
+        return posters;
+    }
+
+    public void setPosters(List<String> posters)
+    {
+        this.posters = posters;
+    }
+
+    public List<Object> getErrors()
+    {
+        return errors;
+    }
+
+    public void setErrors(List<Object> errors)
+    {
+        this.errors = errors;
+    }
+}
