5286b30f1ba675d94bb3689b473ed52b88d27018
roxannethomas <roxthomas@auchan.fr>
2019-06-16 23:05:22 +0200

    Last compilable version before navigation

---
+++ app/src/main/java/com/projet/roxanne_thomas/betaserie/MySerie.java
@@ -0,0 +1,13 @@
+package com.projet.roxanne_thomas.betaserie;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MySerie extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.my_serie_activity);
+    }
+}
